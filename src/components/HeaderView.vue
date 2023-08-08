9e556e42f2b9c38620d2ac48fe8af63be33078ef
Nawal Maqsood <nawalmaqsood16@gmail.com>
2023-08-08 13:49:07 +0500

    Updated

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -4,6 +4,9 @@
           <v-app-bar-nav-icon></v-app-bar-nav-icon>
           <v-toolbar-title>MyTasks</v-toolbar-title>
           <v-spacer></v-spacer>
+          <router-link to="/task-created">
+            <v-btn block color="black">Add a task</v-btn>
+          </router-link>
         </v-toolbar>
       </div>
 </template>
